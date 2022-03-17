8948c4c10246f68e6f3b15475d21b8e23ecec416
Thaseus <thaseus@uw.edu>
2022-03-17 15:24:06 -0700

    Initial commit

---
+++ ParticleMagField.py
@@ -0,0 +1,11 @@
+from scipy.constants import e
+
+B0 = 10e-4 # In Teslas, out of the page
+
+n = 1 # Charge of the particle (in elementary charge)
+
+q = n * e # Converts n to be fundamental charge
+
+v0 = 3.1e4
+
+
